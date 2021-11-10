21110a0f7f3bae23cc5efe9acef5fbfad5c78cf7
Singh Kiran <kiran.singh@student.uhasselt.be>
2021-11-10 13:59:20 +0100

    Merge

--- Server.Api/Dtos/UserDto.cs
+++ Server.Api/Dtos/UserDto.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Server.Api.Controllers
 {
-    public class UserDto
+    public record UserDto
     {
         public string email { get; set; }
         public string password { get; set; }
