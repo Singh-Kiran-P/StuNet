f7823d1c862ef7f2f1e4997438fe4d194f3fa922
LanderM-1951720 <lander.moors@student.uhasselt.be>
2021-11-10 14:15:53 +0100

    structure

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Test from '@pages/test';
+
+const App = () => {
+    return (
+        <Test />
+    );
+};
+
+export default App;
---
+++ client/src/pages/test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {
+    Text
+} from 'react-native';
+
+const Test = () => {
+    return (
+        <Text>Test 3</Text>
+    );
+};
+
+export default Test;
