81a8913b21cf20962ea288d19b9e0f6b526657f7
senn <senn.robyns@student.uhasselt.be>
2021-11-10 13:51:16 +0100

    small fix

--- Server.Api/Repositories/IUserRepository.cs
+++ Server.Api/Repositories/IUserRepository.cs
@@ -5,12 +5,12 @@ using Server.Api.Models;
 
 namespace Server.Api.Repositories
 {
-    public interface IUserRepository
+    public interface IUserRepository: IInterfaceRepository<User>
     {
-         Task<IEnumerable<User>> getAllAsync();
-         Task<User> getAsync(int id);
-         Task createAsync(User user);
-         Task updateAsync(User user);
-         Task deleteAsync(int userId);
+        //  Task<IEnumerable<User>> getAllAsync();
+        //  Task<User> getAsync(int id);
+        //  Task createAsync(User user);
+        //  Task updateAsync(User user);
+        //  Task deleteAsync(int userId);
     }
 }
