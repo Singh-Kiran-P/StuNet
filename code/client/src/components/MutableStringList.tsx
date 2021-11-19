84d61d4105c0e99f66f7c6909f4495a4e624b03a
tijl <tijl.elens@student.uhasselt.be>
2021-11-19 21:46:48 +0100

    style: Define stylesheet in MutableStringList.tsx

--- code/client/src/components/MutableStringList.tsx
+++ code/client/src/components/MutableStringList.tsx
@@ -1,18 +1,49 @@
 import React, {Component, useState} from 'react';
-import {View} from 'react-native';
-import {Text} from 'react-native';
-// import {accessibilityProps} from 'react-native-paper/lib/typescript/components/MaterialCommunityIcon';
+
 import {
-  // ActivityIndicator,
+  // LayoutAnimation,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+import {
   Button,
-  // List,
   TextInput,
+  // ActivityIndicator,
+  // List,
 } from 'react-native-paper';
+import {
+  Theme,
+  // text,
+} from '@css';
 
 type Props = {
   title: string;
   style?: object;
 };
+
+const stylesheet = StyleSheet.create({
+  title: {
+    backgroundColor: Theme.colors.primary,
+    color: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+  input: {
+    marginBottom: 5,
+  },
+  items: {
+      paddingTop: 2,
+  },
+  item: {
+    padding: 2,
+    textAlign: 'center',
+    backgroundColor: 'lightgray',
+    borderRadius: 2,
+    marginTop: 2,
+    marginHorizontal: 2,
+  },
+});
 
 export default function MutuableStringList(props: Props) {
   const [elements, setElements] = useState<string[]>([]);
@@ -35,16 +66,24 @@ export default function MutuableStringList(props: Props) {
     }
   };
 
+  const overwriteStyle: object = {
+    borderWidth: 1,
+    borderColor: Theme.colors.primary,
+    borderRadius: 3,
+    padding: 10,
+    marginTop: 10,
+  };
+  const style: object = {...props.style!, ...overwriteStyle};
+
   return (
-    <View style={props.style}>
-      <TextInput
-        mode="outlined"
-        label={props.title}
-        onChangeText={setElement}
-      />
-      {elements.map((item, i) => {
-        return <Text key={i}>{item}</Text>;
-      })}
+    <View style={style}>
+      <Text style={stylesheet.title}>{props.title}</Text>
+      <View style={stylesheet.items}>
+        {elements.map((item, i) => {
+            return <Text style={stylesheet.item} key={i}>{item}</Text>;
+        })}
+      </View>
+      <TextInput style={stylesheet.input} mode="outlined" label="add" onChangeText={setElement} />
       <View style={{flexDirection: 'row'}}>
         <Button
           mode="contained"
