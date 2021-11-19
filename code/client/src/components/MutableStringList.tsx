9fc224249d4dc1c14261fd93fd4b26e3dc0fe137
tijl <tijl.elens@student.uhasselt.be>
2021-11-19 19:30:20 +0100

    chore: Follow eslint conventions
    * Follow eslint conventions in MutableStringList

--- code/client/src/components/MutableStringList.tsx
+++ code/client/src/components/MutableStringList.tsx
@@ -1,49 +1,55 @@
-import React, {Component, useState} from "react";
-import { View } from "react-native";
-import { Text } from "react-native";
-import { accessibilityProps } from "react-native-paper/lib/typescript/components/MaterialCommunityIcon";
+import React, {Component, useState} from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native';
+// import {accessibilityProps} from 'react-native-paper/lib/typescript/components/MaterialCommunityIcon';
 import {
-    ActivityIndicator,
+    // ActivityIndicator,
     Button,
-    List,
+    // List,
     TextInput,
 } from 'react-native-paper';
 
 type Props = {
-    title: string
-}
+  title: string;
+};
 
 export default function MutuableStringList(props: Props) {
-    
-    const [elements, setElements] = useState<string[]>([]);
-    const [element, setElement] = useState('');
+  const [elements, setElements] = useState<string[]>([]);
+  const [element, setElement] = useState('');
 
-    const add = () =>{
-        elements.push(element)
-        
-        setElements(elements.slice())
-        
-        console.log("ADDING")
-        console.log(elements)
-    }
+  const add = () => {
+    elements.push(element);
 
-    const remove = () =>{
-        if (elements.length > 0){
-            elements.pop()
-            setElements(elements.slice())
-            console.log("Removing")
-        }
-            
+    setElements(elements.slice());
+
+    console.log('ADDING');
+    console.log(elements);
+  };
+
+  const remove = () => {
+    if (elements.length > 0) {
+      elements.pop();
+      setElements(elements.slice());
+      console.log('Removing');
     }
-    return(
-        <View>
-            <TextInput mode='outlined' label={props.title} onChangeText={setElement} />
-            {elements.map((item, i) => {
-                return <Text key={i}>{item}</Text>})
-                }
-            <Button mode='contained' onPress={add} disabled={ element === ''}>Add</Button>
-            <Button mode='contained' onPress={remove}>Remove</Button>
-        </View>
-        
-    )
+  };
+
+  return (
+    <View>
+      <TextInput
+        mode="outlined"
+        label={props.title}
+        onChangeText={setElement}
+      />
+      {elements.map((item, i) => {
+        return <Text key={i}>{item}</Text>;
+      })}
+      <Button mode="contained" onPress={add} disabled={element === ''}>
+        Add
+      </Button>
+      <Button mode="contained" onPress={remove}>
+        Remove
+      </Button>
+    </View>
+  );
 }
