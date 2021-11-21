10d693deeac38481f5619aa00cb030b3af683e81
Melih Demirel <melih.demirel@student.uhasselt.be>
2021-11-21 12:19:22 +0100

    chore: Define CreateCourseDto

--- code/Server.Api/Dtos/CourseDto.cs
+++ code/Server.Api/Dtos/CourseDto.cs
@@ -1,4 +1,6 @@
-
+using System;
+using Server.Api.Models;
+using System.Collections.Generic;
 
 namespace Server.Api.Controllers // controllers?
 {
@@ -6,5 +8,17 @@ namespace Server.Api.Controllers // controllers?
     {
         public string Name { get; set; }
         public string Number { get; set; }
+    }
+
+    public record createCourseDto
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        //public ICollection<string> channels {get; set;}
+
+        public ICollection<string> topicNames {get; set;}
+
+        //ublic ICollection<string> assitents {get; set;} 
     }
 }
