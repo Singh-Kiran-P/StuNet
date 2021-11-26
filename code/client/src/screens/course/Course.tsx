ebe26eacf6d8edaf0509d55287ffcb84d371bdb2
tijl <tijl.elens@student.uhasselt.be>
2021-11-26 09:08:13 +0100

    feat: Add questions to Course.tsx

--- code/client/src/screens/course/Course.tsx
+++ code/client/src/screens/course/Course.tsx
@@ -6,12 +6,14 @@ import React, {
 } from '@/.';
 import {
     List,
-    Text,
     // Checkbox,
 } from 'react-native-paper';
 import {
+    Text,
     LoadingWrapper,
     Button,
+    CompactQuestion,
+    Question,
 } from '@/components';
 // import { Text } from 'react-native-paper/lib/typescript/components/Avatar/Avatar';
 
@@ -25,17 +27,20 @@ type Course = {
     name: string;
     number: string;
     topics: Array<Topic>;
+    questions: Array<Question>;
 }
 
 export default Screen('Course', ({ params, nav }) => {
     const [name, setName] = useState('');
     const [number, setNumber] = useState('');
     const [topics, setTopics] = useState<Array<Topic>>([]);
+    const [questions, setQuestions] = useState<Array<Question>>([]);
 
     const init = (result: {data: Course}) => {
         setName(result.data.name);
         setNumber(result.data.number);
         setTopics(result.data.topics);
+        setQuestions(result.data.questions);
         console.log(result.data);
     };
 
@@ -66,12 +71,29 @@ export default Screen('Course', ({ params, nav }) => {
         );
     };
 
+    const renderQuestionList = (): JSX.Element => {
+        return (
+            <List.Accordion title='Questions' onPress={animate}>
+            {
+                questions.map((question, i) =>
+                    <CompactQuestion
+                        key={i}
+                        question={question}
+                        />
+                )
+            }
+            <Button onPress={() => nav.push('Question', { id: 0 })} children='Question'/>
+            </List.Accordion>
+        );
+    };
+
     //TODO: edit title when screen is being called and name has been fetched
     return (
         <LoadingWrapper func={fetch}>
             {/* Text gives weird errors */}
             <Text>{name} ({number})</Text>
             { renderTopicList() }
+            { renderQuestionList() }
             <Button onPress={() => nav.push('AskQuestion', { courseId: params.id })} children='Ask a question' />
         </LoadingWrapper>
     );
