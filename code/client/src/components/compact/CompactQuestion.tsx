4b373b99556bbc1736b893ddc461ccdcb2fe3236
tijl <tijl.elens@student.uhasselt.be>
2021-11-26 12:15:57 +0100

    chore: Define calculateDateDifference

--- code/client/src/components/compact/CompactQuestion.tsx
+++ code/client/src/components/compact/CompactQuestion.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable curly */
 import React from '@/.'; // 'react-native';
 import {
     View,
@@ -16,10 +17,36 @@ type Props = {
 	question: Question;
 }
 
-export function CompactQuestion(props: Props): JSX.Element { // component
+export function CompactQuestion(props: Props): JSX.Element
+{
     const title: string = props.question.title;
     const body: string = props.question.body;
     const datetime: Date = new Date(props.question.time);
+
+    /**
+     * An hour in milliseconds.
+     */
+    const SECOND: number = 1000;
+    const MINUTE: number = 60 * SECOND;
+    const HOUR: number = 60 * MINUTE;
+    const DAY: number = 24 * HOUR;
+
+    /**
+     * Calculates the difference in milleseconds between a
+     * and b.
+     *
+     * NOTE: Date.now() is behind on the winter time (on my
+     * computer at least).
+     *
+     * @param b The time that will be substracted.
+     * @param a The time that will substract.
+     * @returns the difference in milliseconds.
+     */
+    function calculateDateDifference(b: Date, a: Date): number
+    {
+        /* TODO: take the winter time into account dynamically. */
+        return b.getTime() + HOUR - a.getTime()
+    }
 
     return (
         <View>
