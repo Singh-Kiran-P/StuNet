6013dc73ee2278d6ca1a90b1544411817a559bf0
Jochem Lenaers <jochem.lenaers@student.uhasselt.be>
2021-12-16 13:40:32 +0100

    get & send user mail from jwt token, display message time

--- code/client/src/connection/index.tsx
+++ code/client/src/connection/index.tsx
@@ -1,16 +1,18 @@
-import React, { createContext, useContext, useEffect } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import * as signalR from "@microsoft/signalr";
 import { Children } from '@/util';
+import { useToken } from "@/auth";
 
-const Context = createContext<signalR.HubConnection>(new signalR.HubConnectionBuilder().withUrl("http://10.0.2.2:5000/chat").build())
+const Context = createContext<signalR.HubConnection>()
 export const useConnection = () => useContext(Context)
 
 export default ({ children }: Children) => {
-	const connection = useConnection();
+	const token = useToken()[0];
+	const connection = useState(new signalR.HubConnectionBuilder().withUrl("http://10.0.2.2:5000/chat").build())[0];
 
 	useEffect(() => {
 		connection.start()
-			.catch(err => console.log(err));
+		.catch(err => console.log(err));
 
 		return () => {
 			connection.stop()
--- code/client/src/screens/course/textChannel.tsx
+++ code/client/src/screens/course/textChannel.tsx
@@ -3,8 +3,10 @@ import {  View, TextInput, Text, Loader } from '@/components'
 import { useEffect, useRef, useState } from "react";
 import { FlatList } from "react-native";
 import { useConnection } from "@/connection";
+import { useToken } from "@/.";
 import axios from "axios";
-import { continueStatement } from "@babel/types";
+import jwt_decode from "jwt-decode";
+import moment from 'moment'
 
 enum Alignment { Left, Right };
 
@@ -17,16 +19,33 @@ type Message = {
 type Props = {
 	user: string
 	color: string,
+	time: string,
 	alignment: Alignment,
 };
 
-const Message = extend<typeof View, Props>(View, ({ user, color, alignment, children }) => {
+const prettyPrintDateTime = (date: Date) => {
+	const today = new Date()
+	if (today.getUTCFullYear() == date.getUTCFullYear()) {
+		if (today.getUTCMonth() == date.getUTCMonth()) {
+			if (today.getUTCDay() == date.getUTCDay()) {
+				return moment(date).format('HH:mm')
+			} else {
+				return moment(date).format('D MMM, HH:mm')
+			}
+		}
+	} else {
+		return moment(date).format('D MMM yyyy, HH:mm')
+	}
+}
+
+const Message = extend<typeof View, Props>(View, ({ user, color, time, alignment, children }) => {
 	const align: string = alignment == Alignment.Left ? 'flex-start' : 'flex-end'
 
 	return (
 		<View style={{flex: 1, maxWidth: '70%', backgroundColor: color, borderRadius: 10, padding: 10, marginTop: 5, alignItems: align, alignSelf: align }}>
 			<Text>{user}</Text>
-			<Text style={{ textAlign: alignment == Alignment.Left ? 'left' : 'right'}}>{children}</Text>
+			<Text>{children}</Text>
+			<Text style={{ alignSelf: alignment == Alignment.Left ? 'flex-end' : 'flex-start'}}>{prettyPrintDateTime(new Date(time))}</Text>
 		</View>
 	)
 });
@@ -35,8 +54,8 @@ export default Screen('textChannel', ({ params, nav }) => {
 	const [message, setMessage] = useState('');
 	const [messages, setMessages] = useState<Message[]>([]);
 	const [newMessage, setNewMessage] = useState<Message>();
-	const [username, setUsername] = useState<string>(new Date().getTime().toString());
 	const connection = useConnection();
+	const email = jwt_decode(useToken()[0]).username;
 
 	let [theme] = useTheme();
 	let listRef = useRef();
@@ -71,7 +90,7 @@ export default Screen('textChannel', ({ params, nav }) => {
 	}
 
     const sendMessage = (msg: string) => {
-		connection.invoke('NewMessage', username, msg, params.channel.name, params.channel.id)
+		connection.invoke('NewMessage', email, msg, params.channel.name, params.channel.id)
             .catch(err => console.log(err));
 
 		setMessage('');
@@ -81,7 +100,7 @@ export default Screen('textChannel', ({ params, nav }) => {
 		<Loader load={fetch} style={{flex: 1}}>
 			<FlatList ref={listRef} contentContainerStyle={{ flexGrow: 1, justifyContent: 'flex-end' }} data={messages} onContentSizeChange={() => listRef.current.scrollToEnd()} renderItem={
 				({ item, index }) => ( //TODO: Change alignment & color based on userMail (like Messenger)
-					<Message key={index} user={item.userMail} color={theme.primary} alignment={Alignment.Right}>
+					<Message key={index} user={item.userMail} time={item.dateTime} color={item.userMail === email ? theme.primary : theme.accent} alignment={item.userMail === email ? Alignment.Right : Alignment.Left}>
 						{item.body}
 					</Message>
 				)} />
