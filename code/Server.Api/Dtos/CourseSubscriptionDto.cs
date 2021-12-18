6194238fa18f80d299477aa9523aaaa4b4972dfc
tijl <tijl.elens@student.uhasselt.be>
2021-12-18 18:07:22 +0100

    chore: Define getByIdsCourseSubscriptionDto.id

--- code/Server.Api/Dtos/CourseSubscriptionDto.cs
+++ code/Server.Api/Dtos/CourseSubscriptionDto.cs
@@ -6,11 +6,14 @@ using Server.Api.Models;
 namespace Server.Api.Dtos
 {
     public class getByIdsCourseSubscriptionDto
-    {        
+    {  
+        public int id { get; set; }      
 		public DateTime dateTime { get; set; }
-        public static getByIdsCourseSubscriptionDto convert(CourseSubscription subscription) {
+        public static getByIdsCourseSubscriptionDto convert(CourseSubscription subscription)
+        {
 			return new getByIdsCourseSubscriptionDto
 			{
+                id = subscription.id,
 				dateTime = subscription.dateTime,
 			};
 		}
