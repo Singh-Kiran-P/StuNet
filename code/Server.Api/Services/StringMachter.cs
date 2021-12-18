3aef2cdfc962093769c00da4342227d33dddc8db
Singh Kiran <kiran.singh@student.uhasselt.be>
2021-12-18 13:46:57 +0100

    substring matching

--- code/Server.Api/Services/StringMachter.cs
+++ code/Server.Api/Services/StringMachter.cs
@@ -7,33 +7,31 @@ using FuzzySharp;
 using Server.Api.Dtos;
 using Server.Api.Models;
 
-namespace Server.Api.Services
-{
-    public class StringMatcher
-    {
+namespace Server.Api.Services {
+    public class StringMatcher {
         private static int StringMatchPercent = 45;
-        public static IEnumerable<T> FuzzyMatchObject<T>(IEnumerable<T> obj, string searchString)
-        {
-            Type t = typeof(T);
-            List<T> outList = new List<T>();
+        public static IEnumerable<T> FuzzyMatchObject<T> (IEnumerable<T> obj, string searchString) {
+            Type t = typeof (T);
+            List<T> outList = new List<T> ();
 
-            if (searchString == null)
-                return outList;
-
-            foreach (T item in obj)
-            {
-                switch (item)
-                {
+            foreach (T item in obj) {
+                switch (item) {
                     case GetAllCourseDto dto:
-                        int match_ = Fuzz.Ratio(dto.name, searchString);
-                        Console.WriteLine($"{match_} - {searchString} - {dto.name}");
+                        int match_ = 0;
+                        if (searchString == null)
+                            match_ = 100;
+                        else
+                            match_ = Fuzz.PartialRatio (dto.name, searchString);
+                            // match_ = match_ - (100 - Fuzz.Ratio (dto.name, searchString));
+
+                        Console.WriteLine ($"{match_} - {searchString} - {dto.name}");
                         if (match_ >= StringMatchPercent)
-                            outList.Add(item);
+                            outList.Add (item);
 
                         break;
                     default:
                         // Handle this case.
-                        Console.WriteLine("I don't know what this type is.");
+                        Console.WriteLine ("I don't know what this type is.");
                         break;
                 }
             }
