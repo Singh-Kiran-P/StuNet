c6109273c574a43b67749c0063ca44687d9d976c
LanderM-1951720 <lander.moors@student.uhasselt.be>
2021-12-21 15:56:03 +0100

    course test fix?

--- code/Server.UnitTests/CourseTests.cs
+++ code/Server.UnitTests/CourseTests.cs
@@ -67,8 +67,7 @@ namespace Server.UnitTests
                 name = "random" + random.Next().ToString(),
                 number = random.Next().ToString(),
                 topics = new List<Topic> { new Topic() { name = randomName(), id = randomInt() } },
-                channels = new List<TextChannel> { new TextChannel() { name = randomName(), id = randomInt() } },
-                questions = new List<Question>()
+                channels = new List<TextChannel> { new TextChannel() { name = randomName(), id = randomInt() } }
             };
             _courseRepositoryStub.Setup(repo => repo.getAsync(It.IsAny<int>()))
                 .ReturnsAsync(course);
