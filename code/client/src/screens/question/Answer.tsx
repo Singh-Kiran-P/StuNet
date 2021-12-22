f39e1cc5fae36fdb710ffea588998c0e65ceca7b
tijl <tijl.elens@student.uhasselt.be>
2021-12-22 15:23:30 +0100

    feat: Define AnswerScreen.accept

--- code/client/src/screens/question/Answer.tsx
+++ code/client/src/screens/question/Answer.tsx
@@ -1,14 +1,23 @@
-import React, { Screen, EmptyAnswer, useState, axios, dateString } from '@/.';
-import { View, Text, Icon, Loader, CompactQuestion } from '@/components';
+import React, { Screen, EmptyAnswer, useState, useTheme, axios, dateString } from '@/.';
+import { View, Text, Button, Icon, Loader, CompactQuestion } from '@/components';
+
+import { FAB } from "react-native-paper";
 
 export default Screen('Answer', ({ nav, params: { id } }) => {
     let [answer, setAnswer] = useState(EmptyAnswer);
+    let theme = useTheme()[0];
 
     const fetch = async () => {
         return axios.get('/Answer/' + id).then(res => {
             setAnswer(res.data);
             nav.setParams({ course: res.data.question?.course?.name || '' });
         })
+    }
+
+    const accept = (): void => {
+        axios.put('/Answer/SetAccepted/' + id + '?accepted=' + !answer.isAccepted)
+            .then(() => setAnswer({...answer, isAccepted: !answer.isAccepted}))
+            .catch(e => console.error(e));
     }
 
     return (
@@ -25,6 +34,11 @@ export default Screen('Answer', ({ nav, params: { id } }) => {
                     Download 3 Attachments
                 </Text>
             </View>
+            <FAB
+                style={{ position: 'absolute', margin: 16, right: 0, bottom: 0, backgroundColor: !answer.isAccepted ? 'green' : 'red' }}
+                icon={!answer.isAccepted ? 'check' : 'close'}
+                onPress={ () => accept() }
+            />
         </Loader>
     )
 })
