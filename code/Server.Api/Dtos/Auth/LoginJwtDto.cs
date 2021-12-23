151744ea7302d326f3ff3a53b3fb60016021cf95
tijl <tijl.elens@student.uhasselt.be>
2021-12-23 17:32:39 +0100

    chore: Format LoginJwtDto.cs manually

--- code/Server.Api/Dtos/Auth/LoginJwtDto.cs
+++ code/Server.Api/Dtos/Auth/LoginJwtDto.cs
@@ -1,15 +1,13 @@
 // @Kiran
 
-using System;
-
 namespace Server.Api.Dtos
 {
     public record LoginJwtDto
     {
+        public string JwtBearerToken { get; }
         public LoginJwtDto(string token)
         {
             JwtBearerToken = token;
         }
-        public string JwtBearerToken { get; }
     }
 }
