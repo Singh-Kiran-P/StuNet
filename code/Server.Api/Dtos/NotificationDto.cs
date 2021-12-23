4414b8bb67d3cbedc11199af549f4ff29a7b8a67
tijl <tijl.elens@student.uhasselt.be>
2021-12-23 17:36:49 +0100

    clean: Format NotificationDto.cs

--- code/Server.Api/Dtos/NotificationDto.cs
+++ code/Server.Api/Dtos/NotificationDto.cs
@@ -3,33 +3,35 @@ using Server.Api.Models;
 
 namespace Server.Api.Dtos
 {
-	public class NotificationDto
-	{
-		public int id { get; set; }
-		public int notifierId { get; set; }
-		public string title { get; set; }
-		public string body { get; set; }
-		public DateTime time { get; set; }
+    public class NotificationDto
+    {
+        public int id { get; set; }
+        public int notifierId { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+        public DateTime time { get; set; }
 
-		public static NotificationDto convert(AnswerNotification notification) {
-			return new NotificationDto
-			{
-				id = notification.id,
-				notifierId = notification.answerId,
-				title = notification.answer.title,
-				body = notification.answer.body,
-				time = notification.time
-			};
-		}
-		public static NotificationDto convert(QuestionNotification notification) {
-			return new NotificationDto
-			{
-				id = notification.id,
-				notifierId = notification.questionId,
-				title = notification.question.title,
-				body = notification.question.body,
-				time = notification.time
-			};
-		}
-	}
+        public static NotificationDto convert(AnswerNotification notification)
+        {
+            return new NotificationDto
+            {
+                id = notification.id,
+                notifierId = notification.answerId,
+                title = notification.answer.title,
+                body = notification.answer.body,
+                time = notification.time
+            };
+        }
+        public static NotificationDto convert(QuestionNotification notification)
+        {
+            return new NotificationDto
+            {
+                id = notification.id,
+                notifierId = notification.questionId,
+                title = notification.question.title,
+                body = notification.question.body,
+                time = notification.time
+            };
+        }
+    }
 }
