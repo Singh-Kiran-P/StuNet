4cd09e9c04b5495f8096bc86247e4da3917e5125
tijl <tijl.elens@student.uhasselt.be>
2021-12-23 17:35:37 +0100

    Comment unused (old?) code in /Dtos

--- code/Server.Api/Dtos/User/Request/RegisterUserDto.cs
+++ code/Server.Api/Dtos/User/Request/RegisterUserDto.cs
@@ -1,23 +1,23 @@
 // @Kiran @Senn
 
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
+// using System;
+// using System.ComponentModel.DataAnnotations;
+// using System.Diagnostics.CodeAnalysis;
 
-namespace Server.Api.Dtos //LOOK AT USERDTO IN DTOS FOLDER
-{
-    // [ExcludeFromCodeCoverage]
-    // public record RegisterUserDto
-    // {
-    //     [Required(ErrorMessage = "Email is required")]
-    //     [EmailAddress]
-    //     public string Email { get; set; }
-    //     [Required(ErrorMessage = "Password is required")]
-    //     [DataType(DataType.Password)]
-    //     public string Password { get; set; }
-    //     [DataType(DataType.Password)]
-    //     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    //     public string ConfirmPassword { get; set; }
-    //     public string fieldOfStudy { get; set; }
-    // }
-}
+// namespace Server.Api.Dtos //LOOK AT USERDTO IN DTOS FOLDER
+// {
+//     // [ExcludeFromCodeCoverage]
+//     // public record RegisterUserDto
+//     // {
+//     //     [Required(ErrorMessage = "Email is required")]
+//     //     [EmailAddress]
+//     //     public string Email { get; set; }
+//     //     [Required(ErrorMessage = "Password is required")]
+//     //     [DataType(DataType.Password)]
+//     //     public string Password { get; set; }
+//     //     [DataType(DataType.Password)]
+//     //     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+//     //     public string ConfirmPassword { get; set; }
+//     //     public string fieldOfStudy { get; set; }
+//     // }
+// }
