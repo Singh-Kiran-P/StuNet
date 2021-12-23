b40a38921b207c8daa81577a941092b774f3beac
tijl <tijl.elens@student.uhasselt.be>
2021-12-23 17:53:15 +0100

    clean: Remove unused, old code in User.cs

--- code/Server.Api/Models/User.cs
+++ code/Server.Api/Models/User.cs
@@ -5,10 +5,6 @@ namespace Server.Api.Models
 {
     public class User : IdentityUser
     {
-        // public int id { get; set; }
-        // public string email { get; set; }
-        // public string password { get; set; }
-        // public string salt { get; set; }
         public ICollection<Answer> answers;
     }
 }
