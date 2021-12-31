05c413585c909ae16bf7fdd2c27a29621fbea3e3
Jochem Lenaers <jochem.lenaers@student.uhasselt.be>
2021-12-31 19:36:57 +0100

    Fixed missing includes for subscription repository queries

--- code/Server.Api/Repositories/QuestionSubscriptionRepository.cs
+++ code/Server.Api/Repositories/QuestionSubscriptionRepository.cs
@@ -3,18 +3,25 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using Server.Api.DataBase;
 using Server.Api.Models;
 
 namespace Server.Api.Repositories
 {
-    public class PgQuestionSubscriptionRepository : PgSubscriptionRepository<QuestionSubscription>
+    public class PgQuestionSubscriptionRepository : PgSubscriptionRepository<QuestionSubscription, ICollection<Topic>>
     {
         public PgQuestionSubscriptionRepository(IDataContext context) : base(context) { }
 
         protected override DbSet<QuestionSubscription> GetDbSet()
         {
             return _context.QuestionSubscriptions;
+        }
+
+        protected override IIncludableQueryable<QuestionSubscription, ICollection<Topic>> GetIncludes()
+        {
+            return GetDbSet().Include(n => n.subscribedItem).ThenInclude(q => q.course)
+                            .Include(n => n.subscribedItem).ThenInclude(q => q.topics);
         }
     }
 }
