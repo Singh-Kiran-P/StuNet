9dae40f8668f565a43e50b772717694c74c2499d
Singh Kiran <kiran.singh@student.uhasselt.be>
2022-01-03 22:53:45 +0100

    updated depoly script

--- code/Server.Api/depolyOnHeroku.sh
+++ code/Server.Api/depolyOnHeroku.sh
@@ -1,3 +1,10 @@
+# Flush Database
+dotnet ef database update 0
+dotnet ef migrations remove
+dotnet ef migrations add Initial
+dotnet ef database update
+
+# Make/deploy app
 docker build -t singh/stunet-2021 .
 heroku container:push -a stunet-2021 web
 heroku container:release -a stunet-2021 web
