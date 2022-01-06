0a5b1fd2b9cc50a207bcb628c7961ec288b8de5f
LanderM-1951720 <lander.moors@student.uhasselt.be>
2022-01-06 10:49:25 +0100

    final

--- code/client/src/screens/courses/CreateCourse.tsx
+++ code/client/src/screens/courses/CreateCourse.tsx
@@ -30,8 +30,8 @@ export default Screen('CreateCourse', ({ nav }) => {
         <View>
             <TextInput label='Name' onChangeText={setName}/>
             <TextInput margin label='Number' onChangeText={setNumber}/>
-            <TextInput margin label='Description' multiline onChangeText={setDescription}/>
             <TextInput margin label='Email' onChangeText={setEmail}/>
+            <TextInput margin label='Description' multiline onChangeText={setDescription}/>
             <Button margin icon='book-plus' children='Create' disabled={!name || !number || !email} toggled={error} onPress={submit}/>
             <Text type='error' margin hidden={!error} children={error}/>
         </View>
